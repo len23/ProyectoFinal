1f7e139d2e8eee8a54149d730b1c36d688697a6c
Unknown <lenin.montalvo.23@hotmail.com>
2018-06-05 15:23:08 -0500

    Errores Correguidos

--- DetectorHumo-app/src/app/app.module.ts
+++ DetectorHumo-app/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import {FormsModule} from "@angular/forms";
 import {TableModule} from 'primeng/table';
 import { AppComponent } from './app.component';
 import {MenuComponent} from '/home/lenin/Escritorio/2018A/JavaScript/VSCode/ProyectoFinal/ProyectoFinal/DetectorHumo-app/src/app/menu/menu.component';
 import {TablaComponent} from './tabla/tabla.component'
-
-import {PanelMenuModule} from 'primeng/panelmenu';
+import {PanelModule} from 'primeng/panel';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PanelMenuModule} from 'primeng/panelmenu';
+import {MenuItem} from 'primeng/api';
 
 
 @NgModule({
@@ -16,17 +16,13 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     AppComponent,
     MenuComponent,
     TablaComponent
-    
   ],
   imports: [
     BrowserModule,
-
-    FormsModule
-
-    PanelMenuModule,
     BrowserAnimationsModule,
-    TableModule
-
+    FormsModule,
+    TableModule ,
+    PanelMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
