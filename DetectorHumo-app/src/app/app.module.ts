3712d9dc264278d332e9ab0e2ce32aa7d3aaf2da
Unknown <lenin.montalvo.23@hotmail.com>
2018-06-05 15:01:32 -0500

    Tabla Completa

--- DetectorHumo-app/src/app/app.module.ts
+++ DetectorHumo-app/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {TableModule} from 'primeng/table';
 import { AppComponent } from './app.component';
 import {MenuComponent} from '/home/lenin/Escritorio/2018A/JavaScript/VSCode/ProyectoFinal/ProyectoFinal/DetectorHumo-app/src/app/menu/menu.component';
+import {TablaComponent} from './tabla/tabla.component'
 
 import {PanelMenuModule} from 'primeng/panelmenu';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -11,13 +12,15 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    TablaComponent
     
   ],
   imports: [
     BrowserModule,
     PanelMenuModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
